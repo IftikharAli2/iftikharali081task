ccc67f586aeaa59ec661ebb72047e960b64fdf19
Iftikhar <Iftikhar@DESKTOP-UEBPMDG>
2022-01-11 23:32:55 +0500

    Add project files.

---
+++ iftikharali081task/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace iftikharali081task.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ iftikharali081task/data/storedb.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iftikharali_081auto.data
+{
+    public class storedb : DbContext
+    {
+
+        public storedb(DbContextOptions<storedb> options) : base(options){
+            {
+                Database.EnsureCreated();
+            }
+
+        }
+        public DbSet<storedb> orders { get; set; }
+        
+    }
+}
